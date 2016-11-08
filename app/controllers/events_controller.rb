cccc54e8f2034e5aaf245bca684f97f24550a7cf
Kevin Plaza <kplaza1987@gmail.com>
2016-11-08 15:34:21 -0500

    added features

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,49 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+		@event.update(event_params)
+
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render edit_event_path
+    end
+  end
+
+  def new
+  @event = Event.new
+  end
+
+  def create
+  @event = Event.new(event_params)
+  if @event.save
+    redirect_to event_path(@event)
+  else
+    render new_event_path
+  end
+end
+
+
+  def destroy
+    Event.delete(params[:id])
+  redirect_to url_for(:controller => :events, :action => :index)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title,:description, :start_at, :location, :user_id)
+  end
 end
--- db/migrate/20161108191451_create_events.rb
+++ db/migrate/20161108191451_create_events.rb
@@ -6,6 +6,7 @@ class CreateEvents < ActiveRecord::Migration[5.0]
       t.datetime :start_at
       t.string :location
       t.integer :user_id
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
