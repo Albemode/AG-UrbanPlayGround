a8eb2efa819ce03375b4b4fd0438d19e342f9420
Kevin Plaza <kplaza1987@gmail.com>
2016-11-09 12:26:15 -0500

    5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    puts params
+    @id = params[:id]
+    @user = User.find(@id)
+    @message = "hello user "
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :kids
 
   get '/' => 'welcome#index'
+
+  get "/users/:id" =>  "users#show", as: "user"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
